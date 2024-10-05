6392d4c07360312edd9845405f3755122aec375a
refulgence <oscuritas@yandex.ru>
2024-10-05 19:19:31 +0300

    more cleaning

--- data.lua
+++ data.lua
@@ -4,48 +4,4 @@ require "prototypes/digitizer-chest"
 require "prototypes/digitizer-combinator"
 require "prototypes/dedigitizer-reactor"
 require "prototypes/shortcuts"
-
-data:extend{
-    {type = "sprite",
-	name = "qf-vanilla-ghost-entity-icon",
-	filename = "__core__/graphics/icons/mip/ghost-entity.png",
-	priority = "extra-high-no-scale",
-    flags = {"gui-icon"},
-    mipmap_count = 3,
-    scale = 0.5,
-    size = 64
-    },
-    {type = "sprite",
-    name = "qf-setting-icon",
-    filename = "__quantum-fabricator__/graphics/icons/setting-icon.png",
-    priority = "extra-high-no-scale",
-    flags = {"gui-icon"},
-    scale = 1,
-    size = 32
-    },
-    {type = "sprite",
-    name = "qf-setting-icon-white",
-    filename = "__quantum-fabricator__/graphics/icons/setting-icon-white.png",
-    priority = "extra-high-no-scale",
-    flags = {"gui-icon"},
-    scale = 1,
-    size = 32
-    },
-    {type = "sprite",
-    name = "qf-toggle-storage-icon",
-    filename = "__quantum-fabricator__/graphics/icons/shelf-shelves-icon.png",
-    priority = "extra-high-no-scale",
-    flags = {"gui-icon"},
-    scale = 1,
-    size = 32
-    },
-    {type = "sprite",
-    name = "qf-toggle-storage-icon-white",
-    filename = "__quantum-fabricator__/graphics/icons/shelf-shelves-icon-white.png",
-    priority = "extra-high-no-scale",
-    flags = {"gui-icon"},
-    scale = 1,
-    size = 32
-    },
-
-}
+require "prototypes/sprites"
---
+++ prototypes/sprites.lua
@@ -0,0 +1,44 @@
+data:extend{
+    {type = "sprite",
+	name = "qf-vanilla-ghost-entity-icon",
+	filename = "__core__/graphics/icons/mip/ghost-entity.png",
+	priority = "extra-high-no-scale",
+    flags = {"gui-icon"},
+    mipmap_count = 3,
+    scale = 0.5,
+    size = 64
+    },
+    {type = "sprite",
+    name = "qf-setting-icon",
+    filename = "__quantum-fabricator__/graphics/icons/setting-icon.png",
+    priority = "extra-high-no-scale",
+    flags = {"gui-icon"},
+    scale = 1,
+    size = 32
+    },
+    {type = "sprite",
+    name = "qf-setting-icon-white",
+    filename = "__quantum-fabricator__/graphics/icons/setting-icon-white.png",
+    priority = "extra-high-no-scale",
+    flags = {"gui-icon"},
+    scale = 1,
+    size = 32
+    },
+    {type = "sprite",
+    name = "qf-toggle-storage-icon",
+    filename = "__quantum-fabricator__/graphics/icons/shelf-shelves-icon.png",
+    priority = "extra-high-no-scale",
+    flags = {"gui-icon"},
+    scale = 1,
+    size = 32
+    },
+    {type = "sprite",
+    name = "qf-toggle-storage-icon-white",
+    filename = "__quantum-fabricator__/graphics/icons/shelf-shelves-icon-white.png",
+    priority = "extra-high-no-scale",
+    flags = {"gui-icon"},
+    scale = 1,
+    size = 32
+    },
+
+}

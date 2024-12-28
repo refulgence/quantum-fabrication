ab544bcc7a388609bb76c002469d52ec859452ff
refulgence <oscuritas@yandex.ru>
2024-12-29 00:10:24 +0300

    fix: :bug: fix to_chunk to return correct BoundingBox

--- scripts/chunks_utils.lua
+++ scripts/chunks_utils.lua
@@ -36,11 +36,11 @@ end
 ---@return BoundingBox
 function chunks_utils.to_chunk(position)
     if not position.x then return position end
-    local top_left_x = math.floor(position.x / 32) * 32
-    local top_left_y = math.floor(position.y / 32) * 32
-    local bottom_right_x = top_left_x + 32
-    local bottom_right_y = top_left_y + 32
-    return {top_left = {x = top_left_x, y = top_left_y}, bottom_right = {x = bottom_right_x, y = bottom_right_y}}
+    local left_top_x = math.floor(position.x / 32) * 32
+    local left_top_y = math.floor(position.y / 32) * 32
+    local right_bottom_x = left_top_x + 32
+    local right_bottom_y = left_top_y + 32
+    return {left_top = {x = left_top_x, y = left_top_y}, right_bottom = {x = right_bottom_x, y = right_bottom_y}}
 end
 
 return chunks_utils

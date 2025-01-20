f457f604615c4c74c5fcb36c48c6940f789d8f56
refulgence <oscuritas@yandex.ru>
2025-01-20 18:48:17 +0300

    fix: :bug: fix crash involving building unmineable tiles

--- scripts/process_data.lua
+++ scripts/process_data.lua
@@ -106,7 +106,7 @@ end
 -- This mainly exists to obtain precious items_to_place_this data 
 function process_entities()
     local entities = prototypes.get_entity_filtered({{filter = "buildable"}})
-    local tiles = prototypes.get_tile_filtered({{filter = "minable"}})
+    local tiles = prototypes.tile
     storage.prototypes_data = {}
     if not storage.craft_stats then storage.craft_stats = {} end
     local result = {}

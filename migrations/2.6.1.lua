caaddfab73bc07e17c1a159407acd915c63a1364
refulgence <oscuritas@yandex.ru>
2025-06-25 21:19:36 +0300

    fix: :bug: fix a crash happening during migration for certain requests

--- migrations/2.6.1.lua
+++ migrations/2.6.1.lua
@@ -1,10 +1,12 @@
 local tracking = require("scripts/tracking_utils")
 
 for type, request_table in pairs(storage.tracked_requests) do
-    for id, entry in pairs(request_table) do
-        if entry.entity then
-            if not entry.entity.valid or entry.entity.force.name ~= "player" then
-                tracking.remove_tracked_request(type, id)
+    if type ~= "revivals" and type ~= "destroys" and type ~= "upgrades" then
+        for id, entry in pairs(request_table) do
+            if entry.entity then
+                if not entry.entity.valid or entry.entity.force.name ~= "player" then
+                    tracking.remove_tracked_request(type, id)
+                end
             end
         end
     end
